b9307e0eafa2df54e500f59c7cd5f92bb2a30c40
Cassandra Keddis <cassandra@thinkific.com>
2023-02-02 20:55:26 -0800

    initial form setup

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -171,5 +172,21 @@ storiesOf("DayList", module)
           <Error
             message={"Count not delete appointment."}
             onClose={action("onClose")}
+          />
+        ))
+        .add("Create", () => (
+          <Form 
+            student={"Lydia Miller-Jones "}
+            interviewer={1}
+            interviewers={interviewers}
+            onSave={action("onSave")}
+            onCancel={action("onCancel")}
+          />
+        ))
+        .add("Edit", () => (
+          <Form 
+            interviewers={interviewers}
+            onSave={action("onSave")}
+            onCancel={action("onCancel")}
           />
         ))
