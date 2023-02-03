a0bd114bdd2714618d9bc83b363c79d800f3028f
Cassandra Keddis <cassandra@thinkific.com>
2023-02-02 20:18:37 -0800

    Error component

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -166,5 +167,9 @@ storiesOf("DayList", module)
           />
         ))
         .add("Status", () => <Status message={"Deleting"}/>)
-        
-
+        .add("Error", () => (
+          <Error
+            message={"Count not delete appointment."}
+            onClose={action("onClose")}
+          />
+        ))
