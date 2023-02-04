415739bf66a9b873a41f8730c6f80fa4710315af
Cassandra Keddis <cassandra@thinkific.com>
2023-02-03 18:07:03 -0800

    display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <Fragment>
+      <Header time={props.time}></Header>
+      <article className="appointment">{props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}></Show> : <Empty></Empty>}</article>
+    </Fragment>
   )
 }
