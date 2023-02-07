1a597b4d162fd40b9f6272168ae961ac60bbe918
Cassandra Keddis <cassandra@thinkific.com>
2023-02-06 21:03:36 -0800

    call days api and remove hardcoded days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import axios from "axios";
 
 const appointments = {
   "1": {
@@ -72,6 +55,14 @@ const appointment = Object.values(appointments).map((appointment) => {
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days')
+    .then(function (response) {
+    setDays(response.data);
+  })
+  }, [days])
 
   return (
     <main className="layout">
