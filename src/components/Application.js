44073af955de0745244a03aad16143c53eda99fc
Cassandra Keddis <cassandra@thinkific.com>
2023-02-07 20:32:46 -0800

    retrieve appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,73 +4,42 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from "axios";
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
-const appointment = Object.values(appointments).map((appointment) => {
-  return (
-    <Appointment 
-      key={appointment.id} 
-      {...appointment} 
-    />
-    )
-});
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    // appointments: {}
+    appointments: {}
+  });
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const appointment = dailyAppointments.map((appointment) => {
+    return (
+      <Appointment 
+        key={appointment.id} 
+        {...appointment} 
+      />
+      )
   });
 
   useEffect(() => {
-    axios.get('http://localhost:8001/api/days')
-    .then((response) =>
-      setDays(response.data));
-
+    // axios.get('http://localhost:8001/api/days')
+    // .then((response) =>
+      // setDays(response.data));
+      Promise.all([
+        axios.get('http://localhost:8001/api/days'),
+        axios.get('http://localhost:8001/api/appointments')
+        // axios.get('http://localhost:8001/api/interviewers')
+      ]).then((res) => {
+        setState(prev => ({...prev, days: res[0].data, appointments: res[1].data}))
+      });
   }, []);
 
   const setDay = day => setState({ ...state, day });
 
-  const setDays = (days) => setState(prev => ({ ...prev, days }));
+  // const setDays = (days) => setState(prev => ({ ...prev, days }));
 
   return (
     <main className="layout">
