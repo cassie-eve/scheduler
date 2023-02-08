ae028116edd9440bb9bde248f31e42890e172ffd
Cassandra Keddis <cassandra@thinkific.com>
2023-02-07 19:31:49 -0800

    managing state

--- src/components/Application.js
+++ src/components/Application.js
@@ -54,15 +54,23 @@ const appointment = Object.values(appointments).map((appointment) => {
 });
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    // appointments: {}
+  });
 
   useEffect(() => {
     axios.get('http://localhost:8001/api/days')
-    .then(function (response) {
-    setDays(response.data);
-  })
-  }, [days])
+    .then((response) =>
+      setDays(response.data));
+
+  }, []);
+
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = (days) => setState(prev => ({ ...prev, days }));
 
   return (
     <main className="layout">
@@ -75,9 +83,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
         <DayList 
-          days={days} 
-          value={day} 
-          onChange={setDay} 
+          days={state.days} 
+          value={state.day} 
+          onChange={setDay}
         />
         </nav>
         <img
