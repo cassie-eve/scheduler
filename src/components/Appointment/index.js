e0ffa6e4356f8777e7f82730a693139939ca450a
Cassandra Keddis <cassandra@thinkific.com>
2023-02-08 20:31:52 -0800

    use our custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,12 +4,33 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <Fragment>
       <Header time={props.time}></Header>
-      <article className="appointment">{props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}></Show> : <Empty></Empty>}</article>
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => transition(EMPTY)}
+        />  
+      )}
     </Fragment>
   )
 }
