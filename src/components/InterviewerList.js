7f2ad4e4465fbbf2c15844ec2003a084b3dfe066
Cassandra Keddis <cassandra@thinkific.com>
2023-02-15 20:17:55 -0800

    form and appointment tests

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,18 +1,19 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
-const {interviewers, interviewer} = props;
-const listItem = interviewers.map(person =>{
-  return <InterviewerListItem 
-    key={person.id} 
-    name={person.name} 
-    avatar={person.avatar} 
-    selected={person.id === interviewer}
-    setInterviewer={() => props.setInterviewer(person.id)}
-  />
+  const {interviewers, interviewer} = props;
+  const listItem = interviewers.map(person =>{
+    return <InterviewerListItem 
+      key={person.id} 
+      name={person.name} 
+      avatar={person.avatar} 
+      selected={person.id === interviewer}
+      setInterviewer={() => props.setInterviewer(person.id)}
+    />
   });
   return (
     <section className="interviewers">
@@ -23,3 +24,7 @@ const listItem = interviewers.map(person =>{
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
