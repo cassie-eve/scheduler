213bfd0f0dfb580ce6813645876249dd872cae3a
Cassandra Keddis <cassandra@thinkific.com>
2023-02-18 08:35:53 -0800

    mocking axios call

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -11,9 +11,9 @@ export default function useApplicationData() {
 
   useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then((res) => {
       setState(prev => ({...prev, days: res[0].data, appointments: res[1].data, interviewers: res[2].data}));
     });
@@ -41,7 +41,7 @@ export default function useApplicationData() {
   }
 
   function bookInterview(id, interview) {
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview})
+    return axios.put(`/api/appointments/${id}`, {interview})
 
     .then((res) => {
     
@@ -65,7 +65,7 @@ export default function useApplicationData() {
 
   function cancelInterview(id, interview) {
 
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
+    return axios.delete(`/api/appointments/${id}`)
       .then((res) => { 
 
         const appointment = {
